dc82b55733c21486e2c5d9dbdba17578396fc206
Pedro Palhari <pedropalhari@gmail.com>
2021-01-16 21:15:37 -0300

    Adding comments

--- src/types/ExampleTypes.d.ts
+++ src/types/ExampleTypes.d.ts
@@ -3,6 +3,9 @@
  * @see https://github.com/YousefED/typescript-json-schema/blob/master/api.md
  */
 
+/**
+ * This Example interface can be anywhere in your project
+ */
 export interface Example {
   exampleParam: string;
 }
